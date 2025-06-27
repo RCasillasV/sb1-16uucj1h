f209a1c10d5577dc23f896ba41c514644eda8990
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-06-27 14:29:39 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div style={{ backgroundColor: 'lightblue', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <h1>¡Hola, DoctorSoft!</h1>
-      {/* Aquí agregas el componente LogoutTestPage */}
       <LogoutTestPage />
        <h1>¡Bye, DoctorSoft!</h1>
     </div>
