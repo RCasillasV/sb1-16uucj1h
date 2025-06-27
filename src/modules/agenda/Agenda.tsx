6d2019b7baf8adb859f539ff6e161484ab70630c
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-06-26 18:29:49 -0600

    Updated Agenda.tsx

--- src/modules/agenda/Agenda.tsx
+++ src/modules/agenda/Agenda.tsx
@@ -330,7 +330,7 @@ export function Agenda() {
               <style>
                 {`
                   .fc-timegrid-slot-label {
-                    width: 70px !important;
+                    width: 60px !important;
                     font-size: 0.75rem !important;
                    color: ${currentTheme.colors.text} !important;
                   }
@@ -339,7 +339,7 @@ export function Agenda() {
                    color: ${currentTheme.colors.text} !important;
                   }
                   .fc .fc-timegrid-slot-label-cushion {
-                    width: 70px !important;
+                    width: 60px !important;
                     font-size: 0.75rem !important;
                    color: ${currentTheme.colors.text} !important;
                   }
