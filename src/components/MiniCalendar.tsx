098e746e6e9a905949e3d70770fdd7a0fdbceee9
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-06-26 19:45:54 -0600

    Updated MiniCalendar.tsx

--- src/components/MiniCalendar.tsx
+++ src/components/MiniCalendar.tsx
@@ -230,7 +230,7 @@ export function MiniCalendar({ selectedDate, onDateSelect, events, currentViewDa
                       className="text-[9px] truncate leading-tight"
                       style={{ color: currentTheme.colors.textSecondary }}
                     >
-                     {event.extendedProps?.tipo_consulta} {event.extendedProps?.reason}:                      {event.extendedProps?.tiempo_evolucion} {event.extendedProps?.unidad_tiempo}  
+                     {event.extendedProps?.reason}:                      {event.extendedProps?.tiempo_evolucion} {event.extendedProps?.unidad_tiempo}  
                     </p>
                   </div>
                 </div>
