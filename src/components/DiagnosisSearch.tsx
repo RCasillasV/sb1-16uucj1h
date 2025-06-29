a90b4f3531d31be91383bb4f38d8508ce570a976
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-06-29 15:44:45 -0600

    Updated DiagnosisSearch.tsx

--- src/components/DiagnosisSearch.tsx
+++ src/components/DiagnosisSearch.tsx
@@ -127,7 +127,7 @@ export function DiagnosisSearch({ selectedDiagnoses, onSelect, onRemove }: Diagn
         const searchTerms = searchTerm.toLowerCase().split(' ');
         
         const { data, error } = await supabase
-          .from('sires.tcCIE10')
+          .from('tcCIE10', {schema: ´sires´})
           .select('Consecutivo, Catalog_Key, Nombre')
           .or(searchTerms.map(term => `Catalog_Key.ilike.%${term}%,Nombre.ilike.%${term}%`).join(','))
           .limit(10);
