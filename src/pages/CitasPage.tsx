ef6abaefca5e6436909e4f90257e17d76f45524b
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-01 09:49:12 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -397,6 +397,14 @@ export function CitasPage() {
                         }
                       }}
                     />
+                    <button 
+                      type="button"
+                      onClick={handleAddSymptom}
+                      className={buttonStyle.base}
+                      style={buttonStyle.primary}
+                    >
+                      Agregar
+                    </button>
                   </div>
                 </div>
               </div>
