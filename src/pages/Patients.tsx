e60324fd9685f47b4cab9cf0143a1d0144e91b4e
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-01 10:52:34 -0600

    Updated Patients.tsx

--- src/pages/Patients.tsx
+++ src/pages/Patients.tsx
@@ -67,7 +67,7 @@ export function Patients() {
   }
 
   return (
-    <div>
+    <div className="w-full mx-auto space-y-2">
       <div className="flex justify-between items-center mb-1">
         <div className="flex items-center gap-2">
           <Users className="h-6 w-6" style={{ color: currentTheme.colors.text }} />
