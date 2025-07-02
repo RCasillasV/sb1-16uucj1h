6ef30282ea51d781a4cc97563431046f65943b87
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-02 13:58:34 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -145,7 +145,7 @@ export function CitasPage() {
           p_fechanac: format(birthDate, 'yyyy-MM-dd'), 
           p_especialidad: 'Ginecólogo' 
         });    
-        console.log("Fecha Nacimiento:",p_fechanac, "Especialidad médica:",p_especialidad)
+        console.log("Fecha Nacimiento:",birthDate, "Especialidad médica:",p_especialidad)
         if (error) throw error;
         // Update state with the fetched symptoms
         setDynamicSymptoms(data || []);
