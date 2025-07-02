54e9095c61e31ea52ab12885161e36386c28aac6
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-02 14:35:18 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -143,7 +143,7 @@ export function CitasPage() {
         // Call the RPC function
         const { data, error } = await supabase.rpc('sintomasconsulta', { 
           p_fechanac: format(birthDate, 'yyyy-MM-dd'), 
-          p_especialidad: 'Ginecólogo' 
+          p_especialidad: 'Ortopedista' 
         });    
         console.log("Fecha Nacimiento:",format(birthDate, 'yyyy-MM-dd'), "Especialidad médica:")
         if (error) throw error;
