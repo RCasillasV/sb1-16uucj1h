cc5f915839db38ff1784323844f1a6a3c7396515
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-02 14:49:20 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -143,7 +143,7 @@ export function CitasPage() {
         // Call the RPC function
         const { data, error } = await supabase.rpc('sintomasconsulta', { 
           p_fechanac: format(birthDate, 'yyyy-MM-dd'), 
-          p_especialidad: 'Oftalmólogo ' 
+          p_especialidad: 'Oftalmólogo' 
         });    
         if (error) throw error;
         // Update state with the fetched symptoms
