5f9f2672840ae2326a3143ba481da2d6980396f2
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-02 15:25:35 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -143,7 +143,7 @@ export function CitasPage() {
         // Call the RPC function
         const { data, error } = await supabase.rpc('sintomasconsulta', { 
           p_fechanac: format(birthDate, 'yyyy/MM/dd'), 
-          p_especialidad: 'Gastroenterólogo' 
+          p_especialidad: 'Preescolar' 
         });    
         if (error) throw error;
         // Update state with the fetched symptoms
