90f8336c580313924c745c7c0fc374c466522515
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-02 12:52:44 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -143,7 +143,7 @@ export function CitasPage() {
         // Call the RPC function
         const { data, error } = await supabase.rpc('sintomasconsulta', { 
           p_fechanac: format(birthDate, 'yyyy-MM-dd'), 
-          p_especialidad: '' 
+          p_especialidad: 'Nutriologo' 
         });     
         if (error) throw error;
         // Update state with the fetched symptoms
