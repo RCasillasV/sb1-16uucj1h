59cae91d4bc689a12fe2d6e1fb6e665fa4f154ea
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-17 11:02:43 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -270,6 +270,7 @@ export function CitasPage() {
         const { data, error } = await supabase.rpc('sintomasconsulta', { 
           p_fechanac: format(new Date(selectedPatient.FechaNacimiento), 'yyyy/MM/dd'), 
           p_especialidad: specialty
+           console.log('fetchSymptoms: Cadena de fecha enviada a RPC (p_fechanac):', dateToSendToRpc);
         });    
         
         if (error) {
