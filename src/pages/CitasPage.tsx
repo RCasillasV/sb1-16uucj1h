0afb66f32a6fb690f5eae38a93ebbe68d1a7b43f
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-17 11:03:35 -0600

    Updated CitasPage.tsx

--- src/pages/CitasPage.tsx
+++ src/pages/CitasPage.tsx
@@ -269,10 +269,9 @@ export function CitasPage() {
 
         const { data, error } = await supabase.rpc('sintomasconsulta', { 
           p_fechanac: format(new Date(selectedPatient.FechaNacimiento), 'yyyy/MM/dd'), 
-          p_especialidad: specialty,
-           console.log('fetchSymptoms: Cadena de fecha enviada a RPC (p_fechanac):', dateToSendToRpc);
+          p_especialidad: specialty
         });    
-        
+        console.log('fetchSymptoms: Cadena de fecha enviada a RPC (p_fechanac):', dateToSendToRpc);
         if (error) {
           console.error('fetchSymptoms: Error from sintomasconsulta RPC:', error);
           throw error; // Re-throw to be caught by the outer catch block
