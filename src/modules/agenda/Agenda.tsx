b52539b684a101aefff0122d691aaa64aefb7b13
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-18 08:18:21 -0600

    Updated Agenda.tsx

--- src/modules/agenda/Agenda.tsx
+++ src/modules/agenda/Agenda.tsx
@@ -353,6 +353,7 @@ export function Agenda() {
                   }
                   .fc .fc-toolbar-title {
                     color: ${currentTheme.colors.text};
+                    font-size: 1em !important;
                   }
                   .fc .fc-col-header-cell {
                     color: ${currentTheme.colors.textSecondary};
