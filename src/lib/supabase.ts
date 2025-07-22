dfb6455bb417a75b1bfdb5c788b2d51768419f26
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-22 14:28:07 -0600

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -18,7 +18,7 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables. Please check your .env file.');
 }
-
+/*
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: true,
