a71f3eccadd34a3cbdd6c8086dc73fcd85b369d2
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-24 11:31:10 -0600

    Updated MainHeader.tsx

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -301,7 +301,7 @@ export function MainHeader({
             Bienvenido : 
           </span>
           <span className="font-mono" style={{ color: currentTheme.colors.text }}>
-            {userdata.nombre|| 'No identificado'}
+            {userinfo.nombre|| 'No identificado'}
           </span>
         </div>
         <div className="flex flex-col gap-1">
