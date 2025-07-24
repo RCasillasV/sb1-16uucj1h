fa61248171a60b2d0058d2b3b5f7470eabf90979
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-24 12:01:38 -0600

    Updated MainHeader.tsx

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -319,7 +319,7 @@ export function MainHeader({
             Unidad de Negocio:
           </span>
           <span className="font-medium" style={{ color: currentTheme.colors.text }}>
-            {userInfo?.business_unit?.nombre || 'No asignada'}
+            {userInfo?.business_unit?.Nombre || 'No asignada'}
           </span>
         </div>
       </div>
