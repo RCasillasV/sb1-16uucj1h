1ccbfcce10293cf5c5860b53d1a5a301e6496efa
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-07-30 12:31:25 -0600

    Updated Prescriptions.tsx

--- src/pages/Prescriptions.tsx
+++ src/pages/Prescriptions.tsx
@@ -508,7 +508,7 @@ export function Prescriptions() {
                 Agregar Medicamento
               </h4>
 
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+              <div className="grid grid-cols-1 md:grid-cols-6 gap-3">
                 <div className="relative">
                   <label className="block text-xs font-medium mb-1" style={{ color: currentTheme.colors.text }}>
                     Nombre del Medicamento *
