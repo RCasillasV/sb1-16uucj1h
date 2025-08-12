0b0ea173562e288529fcc630ae117a037e777a37
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-08-12 12:27:00 -0600

    Updated PatientForm.tsx

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -535,7 +535,7 @@ export function PatientForm({ onSuccess, onCancel, patient }: PatientFormProps) 
               <div>
                 <label htmlFor="CURP" className="block mb-1 font-medium" style={labelStyle}>
                   <span style={{ color: currentTheme.colors.text }}>CURP</span>
-                  <span style={{ color: currentTheme.colors.secondary}}> a 18 posiciones</span>
+                  <span style={{ color: currentTheme.colors.textsecondary}}> a 18 posiciones</span>
                 </label>
                 <input
                   autoFocus 
