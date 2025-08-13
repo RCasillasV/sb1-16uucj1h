e95d836b4a68b7c7c4b6ee552734907b1073c033
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-08-13 14:39:58 -0600

    Updated PatientForm.tsx

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -176,7 +176,6 @@ export function PatientForm({ onSuccess, onCancel, patient }: PatientFormProps) 
       }
     };
     fetchUserInfo();
-  }, []);
 
     const fetchFederalEntities = async () => { // <-- Este useEffect
       try {
