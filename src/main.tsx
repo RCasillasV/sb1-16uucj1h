3521f0722714a607847c40685b3b8f39387b20be
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-08-15 18:49:04 -0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { AuthProvider } from './contexts/AuthContext'; // 👈 Importa el provider
