c8d8f9756d115d357b4798ea6e319be126319f7d
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-08-19 11:17:56 -0600

    Updated constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_BU = '00000000-0000-0000-0000-000000000000';
-const DEFAULT_ROL = 'Medico';
-const DEFAULT_EDO = 'Activo';
+export const DEFAULT_ROL = 'Medico';
+export const DEFAULT_EDO = 'Activo';
