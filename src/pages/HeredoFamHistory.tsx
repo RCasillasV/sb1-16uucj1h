148e9ef75d9148de432c4508f9896ac9a952028a
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-08-18 20:07:05 -0600

    Updated HeredoFamHistory.tsx

--- src/pages/HeredoFamHistory.tsx
+++ src/pages/HeredoFamHistory.tsx
@@ -509,7 +509,7 @@ export function HeredoFamHistory() {
             Arrastre las patologías desde aquí hacia la fila del familiar correspondiente
           </p>
           
-          <div className="flex flex-wrap gap-2">
+          <div className="flex flex-wrap gap-1.5">
             {globalSelectedCatalogPatologies.map((patology) => (
               <DraggablePathologyTag
                 key={patology.id}
