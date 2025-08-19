d57f44379b7511a3d6e54b7d1d86f2f0d6870a60
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-08-18 21:06:29 -0600

    Fix useMemo import error

--- src/pages/HeredoFamHistory.tsx
+++ src/pages/HeredoFamHistory.tsx
@@ -1,5 +1,5 @@
 // src/pages/HeredoFamHistory.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
