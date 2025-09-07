5c717392473e7bcdc644a77a723858a591ab18f3
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-09-07 13:48:17 -0600

    Updated AntecedentesNoPatologicos.tsx

--- src/pages/AntecedentesNoPatologicos.tsx
+++ src/pages/AntecedentesNoPatologicos.tsx
@@ -376,7 +376,7 @@ export function AntecedentesNoPatologicos() {
                     Consumo de Sustancias
                   </h3>
                   
-                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                  <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
                     <div className="space-y-2">
                       <label 
                         className="block text-sm font-medium"
@@ -456,7 +456,7 @@ export function AntecedentesNoPatologicos() {
                   </div>
 
                   {/* Drogas y Actividad Física */}
-                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                  <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4">
                     <div className="space-y-2">
                       <label 
                         className="block text-sm font-medium"
