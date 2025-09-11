6e3da9c3af4045a8e802ed306efcd9aaee0f3360
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-09-11 15:38:46 -0600

    Updated AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,3 +1,5 @@
+// src/context/AuthContext.tsx
+
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabase';
