601c0207861b77fb96d7e5e6aac8162526dc81d4
Rodolfo Casillas <90527968+RCasillasV@users.noreply.github.com>
2025-09-26 16:10:14 -0600

    Updated api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -445,7 +445,7 @@ const stats = {
           .eq('fecha_cita', new Date().toISOString().split('T')[0]),
         supabase.from('tcCitas').select('id', { count: 'exact' })
           .gte('fecha_cita', new Date().toISOString().split('T')[0])
-          .eq('estado', 'programada')
+          .eq('estado', 'Programada')
       ]);
 
       return {
